2730d514e19a8330a9d4e3fdeb880e54ad96b7d5
Michael Bonner <michaelwbonner@gmail.com>
2018-12-31 12:28:35 -0700

    add headers to allow url checking

--- app/Classes/UrlChecker.php
+++ app/Classes/UrlChecker.php
@@ -31,7 +31,18 @@ class UrlChecker
         try {
             $res = $client->request('GET', $url, [
                 'connect_timeout' => 10,
-                'verify' => false
+                'verify' => false,
+                'headers' => [
+                    'Accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+                    'Accept-Encoding' => 'gzip, deflate',
+                    'Accept-Language' => 'en-US,en;q=0.9',
+                    'Cache-Control' => 'no-cache',
+                    'Connection' => 'keep-alive',
+                    'Host' => (parse_url($url))['host'],
+                    'Pragma' => 'no-cache',
+                    'Upgrade-Insecure-Requests' => '1',
+                    'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
+                ]
             ]);
 
             // retrieve Redirect URI history
