26f23c1ae478f5fc4c283438d1e9bb0669667b43
Michael Bonner <michaelwbonner@gmail.com>
2020-09-01 17:19:47 -0600

    add purgecss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
-  theme: {
-    extend: {},
-  },
-  variants: {},
-  plugins: [],
-}
+    purge: ["./resources/**/*.blade.php", "./resources/**/*.vue"],
+    theme: {
+        extend: {}
+    },
+    variants: {},
+    plugins: [],
+    future: {
+        removeDeprecatedGapUtilities: true
+    }
+};
