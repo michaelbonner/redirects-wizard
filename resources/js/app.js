1899d0c5f99cb5692d915c8ef85e5e46f872a3e4
Michael Bonner <michaelwbonner@gmail.com>
2025-04-22 14:44:22 -0600

    use imports instead of require

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,29 +7,22 @@ import Vue from "vue/dist/vue.common.js";
 
 import "./bootstrap";
 
+import BatchComponent from "./components/BatchComponent.vue";
+import BatchEditComponent from "./components/BatchEditComponent.vue";
+import UrlComponent from "./components/UrlComponent.vue";
+import AddUrlsComponent from "./components/AddUrlsComponent.vue";
+
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component(
-    "batch-component",
-    require("./components/BatchComponent.vue").default
-);
-Vue.component(
-    "batch-edit-component",
-    require("./components/BatchEditComponent.vue").default
-);
-Vue.component(
-    "url-component",
-    require("./components/UrlComponent.vue").default
-);
-Vue.component(
-    "add-urls-component",
-    require("./components/AddUrlsComponent.vue").default
-);
+Vue.component("batch-component", BatchComponent);
+Vue.component("batch-edit-component", BatchEditComponent);
+Vue.component("url-component", UrlComponent);
+Vue.component("add-urls-component", AddUrlsComponent);
 
 const app = new Vue({
-    el: "#app"
+    el: "#app",
 });
