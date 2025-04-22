638f0c18e79177fd8a80efbe304e7dff97bf4a1c
Michael Bonner <michaelwbonner@gmail.com>
2025-04-22 14:53:20 -0600

    add resources/css/app.css to vite config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,11 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue2';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import vue from "@vitejs/plugin-vue2";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/js/app.js',
-            ],
+            input: ["resources/js/app.js", "resources/css/app.css"],
             refresh: true,
         }),
         vue({
