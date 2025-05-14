8a8b2b3f05689b2849eb54a58908c0e484b77732
Michael Bonner <mike@bootpackdigital.com>
2025-05-13 21:35:24 -0600

    composer updates

--- database/factories/BatchFactory.php
+++ database/factories/BatchFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -29,6 +30,7 @@ class BatchFactory extends Factory
             'dev_url' => function () use ($sites) {
                 return $sites->random();
             },
+            'user_id' => User::factory()->create()->id,
         ];
     }
 }
