1f77fc2e0d57d0664974cf7ecd52708c874ad12a
Michael Bonner <mike@bootpackdigital.com>
2026-06-09 16:22:47 -0600

    Add EasyCustomerFeedback widget for logged-in users

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import "../app.css";
     import { authClient } from "$lib/auth-client";
+    import { onMount } from "svelte";
 
     let { data, children } = $props();
 
@@ -8,6 +9,26 @@
         await authClient.signOut();
         window.location.href = "/login";
     }
+
+    onMount(() => {
+        if (!data.user) return;
+
+        const script = document.createElement("script");
+        script.async = true;
+        script.src =
+            "https://easycustomerfeedback.com/widget/d496de302110410cb05eb1b436b7e53e/embed";
+        script.dataset.label = "Send feedback";
+        script.dataset.position = "right";
+        script.dataset.color = "#007595";
+        script.dataset.name = data.user.name;
+        script.dataset.email = data.user.email;
+        script.dataset.userId = data.user.id;
+        document.body.appendChild(script);
+
+        return () => {
+            script.remove();
+        };
+    });
 </script>
 
 <svelte:head>
